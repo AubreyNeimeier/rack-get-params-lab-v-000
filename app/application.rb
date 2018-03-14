bfb419c660d52fc8a23404340613c6e6ef5b9f99
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-14 15:02:48 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -17,12 +17,12 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      if @@items
+      if @@items.empty?
+        resp.write "Your cart is empty"
+      else
         @@items.each do |item|
           resp.write "#{item}\n"
         end
-      elsif
-        resp.write "Your cart is empty"
       end
     elsif req.path.match(/add/)
       item_to_add = req.params["item"]
