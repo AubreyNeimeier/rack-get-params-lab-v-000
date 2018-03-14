01fb3014a369e2c2cf0dd5cb9ee0e87196c00bb9
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-14 14:41:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -27,9 +27,7 @@ class Application
     else
       resp.write "Path Not Found"
     end
-
-    resp.finish
-  end
+  resp.finish
 
   def handle_search(search_term)
     if @@items.include?(search_term)
